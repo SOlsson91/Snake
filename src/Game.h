ca4355a455a9f1aac29e271e898603bbbab92d7b
Sebastian Olsson <me@sebastianolsson.com>
2020-08-15 21:52:40 +0200

    Made a variable const

--- src/Game.h
+++ src/Game.h
@@ -13,7 +13,7 @@ const int MAP_HEIGHT = 20;
 class Game
 {
 private:
-	double m_UpdatedPerSec = 0.1;
+	const double m_UpdatedPerSec = 0.1;
 	const unsigned int CONVERT_TO_SECONDS = 1'000'000;
 public:
 	Game();
